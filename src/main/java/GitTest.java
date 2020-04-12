5de92a51e864f65521490d5cf2da7fe400c63644
ljz <ljz@sina.com>
2020-04-12 20:08:21 +0800

    再一次尝试!!!

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author JunZhe Liu
+ * @Date 2020/4/12 20:07
+ * @Version 2.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我就不行连不上!!!");
+    }
+}
